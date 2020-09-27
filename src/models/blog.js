5b208eb956bbc411bdf9f1e954efe6e791e4194b
GrimmLck <rhmtofficial@gmail.com>
2020-09-27 11:17:08 +0700

    Model dengan Mongoose

---
+++ src/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const BlogPost = new schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Object,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose('BlogPost', BlogPost);
