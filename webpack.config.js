eca36d0149ade747442e9080860ae2453fab9920
GrimmLck <rhmtofficial@gmail.com>
2021-07-21 23:07:21 +0700

    edit anu

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, '/index.js'),
+  entry: path.resolve(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+  mode: 'production',
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  }
 };
