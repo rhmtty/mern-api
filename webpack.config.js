7f238561c0ef3061175a40a1a8732c4c5a225970
GrimmLck <rhmtofficial@gmail.com>
2021-07-21 22:35:53 +0700

    edit webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: path.resolve(__dirname, '/index.js'),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
