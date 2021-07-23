f4ee469644bf62f260234a01157a2a80e18355d7
GrimmLck <rhmtofficial@gmail.com>
2021-07-23 07:32:25 +0700

    add folder images

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  target: 'node',
-  entry: './index.js',
+  target: "node",
+  entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-  mode: 'production',
+  mode: "production",
   resolveLoader: {
-    modules: [
-      __dirname, '/node_modules/'
-    ]
+    modules: [__dirname, "/node_modules/"],
   },
-}
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "images"),
+          to: path.resolve(__dirname, "dist/"),
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+};
